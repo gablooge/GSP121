690b8aa557f4d2b28e95abde3fb3c44d4d66e1f6
Samsul Hadi <samsulhadikediri@gmail.com>
2021-03-12 15:09:50 +0700

    first commit

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+gcloud auth revoke --all
+
+while [[ -z "$(gcloud config get-value core/account)" ]]; 
+do echo "waiting login" && sleep 2; 
+done
+
+while [[ -z "$(gcloud config get-value project)" ]]; 
+do echo "waiting project" && sleep 2; 
+done
+
+
+gcloud source repos create REPO_DEMO
---
+++ README.md
@@ -0,0 +1,10 @@
+# GSP121
+
+Cloud Source Repositories: Qwik Start
+
+https://www.qwiklabs.com/focuses/1002?parent=catalog
+
+# Terminal 1
+> git clone https://github.com/gablooge/GSP121.git
+
+> cd GSP121
